d884b13b84358937c859890474101edbe0c0e4b0
CZT <Ailun72@outlook.com>
2025-01-01 17:38:22 +0800

    1. 编译命令里添加了 -v 参数，可以监控编译过程 2. 正确解决了资源脚本指定编译器的问题, 也解决了编码问题

--- Include/resouse.hpp
+++ Include/resouse.hpp
@@ -1,22 +1,29 @@
-﻿#pragma once
+#pragma once
 #include <windows.h>
-#define ATTRIBUTE TEXT("  Each cell in the cellular automaton has a certain wealth value (abbreviated as W). There is also a commitment value (abbreviated as C) between each cell on the map. For both parties belonging to the same pair, this commitment value is the same.")
-#define ABOUT TEXT("   A cellular automaton is a dynamic system that is discrete in both time and space. Each cellular automaton consists of a regular grid of cell units (cells). Each cell unit has k possible states, and its current state is determined by its own and the previous states of the surrounding cell units.")
-#define ACCELERATOR TEXT("New:\t\t\tCtrl+O\nSave:\t\t\tCtrl+C\nLoad:\t\t\tCtrl+G\nProperties:\t\t\tCtrl+A\nAbout:\t\t\tCtrl+R\n\
-Shortcuts:\t\tCtrl+S\nHorizontal Scrollbar to Zero:\thome\nHorizontal Scrollbar to Max:\tend\nVertical Scrollbar to Zero:\tShift+home\nVertical Scrollbar to Max:\
-\tShift+end\nGO:\t\t\tEnter\nSTOP:\t\t\tSpace\nImplementation Idea:\t\tCtrl+I\nImplementation Tools:\t\tCtrl+T")
-#define SAVE TEXT("Do you want to save the current file?")
-#define IDEA TEXT("    Most of this program is implemented using Windows API without using third-party libraries. During the implementation process, Visual Studio Code was used as the editor, g++ as the compiler, and a little bit of HTML and XML was used.\
-\n    The main.cpp file is the main code file, which implements most of the program's GUI. The Include folder contains three header files: Caculate.hpp, Cell.hpp, and Evolution.h, which define classes and functions related to calculation paths, cell bodies, and evolution, respectively. Correspondingly, there are three source files in the Src folder: Caculate.cpp, Cell.cpp, and Evolution.cpp, which implement related functions. The Resource folder contains the resource.rc file and main.exe.xml file. The former is a resource script file that defines all dialogs, menus, window visual styles, and some shortcuts. The latter is written in XML and, together with the .rc file, is used to specify the window's visual style.\
-The Cell.txt file in the Data folder is used to store save data. The .o files in the Build folder are object files generated during the compilation process.\n\
-    The implementation idea of this program is to first define a cell body class, which contains information such as the cell's position, state, wealth value, and color. The visual representation of the cell's wealth (Wealth) is expressed using RGB: for a certain Wealth value, its color is RGB(max(0, 255-Wealth), min(255, max(0, 510-Wealth)), min(255, max(0, 765-Wealth))).\
-From this, it can be seen that the maximum wealth value of a cell is 765, and the minimum value is 0. The richer the cell, the closer its color is to black, and the poorer it is, the closer it is to white. During the transition from poor to rich, the color changes from white to blue and then to black. The entire drawable map is 50x50, which cannot fit on the screen, so vertical and horizontal scrollbars are added.\
-The window is divided into two parts: the data area on the left and the evolution area on the right. Due to limited energy, the window layout was not considered when the window size changes, so the window is set to a non-resizable state by intercepting the corresponding messages.\n     \
-During the evolution process, if one cell wants to influence another cell, it will transmit the influence through the shortest path between the two cells. Therefore, before the evolution begins, a thread is created using the CreateThread function (the thread function is the Calcution function). The function of this thread is to use breadth-first search to calculate the shortest path between any two living cells and store the results in an adjacency matrix ChannelGraph. This adjacency matrix is a global variable. During the evolution process, each cell can access this matrix to quickly query the shortest path between it and other living cells, thereby influencing other cells. The relationship between two living cells is not only the shortest path but also the commitment value (ranging from 0 to 1). This commitment value dynamically changes during the evolution process. To quickly access it, an adjacency matrix is also used to record it. The core of this cellular automaton is the continuous evolution of living cells. This task is completed by another thread, and the thread function is the Evolution function. Its function is to continuously interact between living cells. The program is set to evolve once every 0.1 seconds and refresh the page once. The evolution process is to randomly select one-fifth of the living cells as the initiators of the evolution, and then select the optimal objects belonging to them from other living cells according to the rules in the problem for interaction. For the details of the interaction, see the problem.\n\
-    The reason for creating two threads for calculation and evolution is that these two tasks are time-consuming and will block the main thread for a long time. The main thread mainly performs GUI rendering. By interacting with the main thread GUI, the other two threads can be controlled to start and stop at any time.\
-In the implementation process of this program, thread safety is involved because these three threads will simultaneously access and modify multiple global variables. This is solved using EnterCriticalSection and LeaveCriticalSection.")
+#define ATTRIBUTE TEXT("  细胞自动机里的每个细胞都具有一定的财富值(简称W) 存在于地图上的每个细胞之间还会\
+存在承诺值(简称C) 对于属于同一对的双方来说 这个承诺值是一样的")
+#define ABOUT TEXT("   细胞自动机是一个时间和空间都离散的动态系统 每个细胞自动机都是由细胞单元(cell)组成的规则网格 \
+每个细胞元都有 k 个可能的状态 其当前状态由其本身及周围细胞元的前一状态共同决定.")
+#define ACCELERATOR TEXT("新建:\t\t\tCtrl+O\n保存:\t\t\tCtrl+C\n读取:\t\t\tCtrl+G\n属性:\t\t\tCtrl+A\n关于:\t\t\tCtrl+R\n\
+快捷键:\t\tCtrl+S\n水平滚动条置零:\thome\n水平滚动条置最大:\tend\n垂直滚动条置零:\tShift+home\n垂直滚动条置最大:\
+\tShift+end\nGO:\t\t\tEnter\nSTOP:\t\t\tSpace\n实现思路:\t\tCtrl+I\n实现工具:\t\tCtrl+T")
+#define SAVE TEXT("是否保存当前文件?")
+#define IDEA TEXT("    该程序绝大部分是使用Windows API来实现的 没有使用到第三方库 在实现过程中 使用Visual Studio Code作为编辑器 g++作为编译器 用到了一点点html和xml。\
+\n    main.cpp文件是最主要的代码文件 它实现了程序大部分的GUI。Include文件夹下包涵的Caculate.hpp Cell.hpp Evolution.h三个头文件分别定义了计算路径、细胞体、演化有关的类和函数 \
+对应的在Src文件夹下有Caculate.cpp Cell.cpp Evolution.cpp三个源文件来实现相关函数功能。Resource文件夹下包含了resource.rc文件和main.exe.xml两个文件 \
+前者是的资源脚本文件 这里定义了所有的对话框 菜单栏 窗口的视觉样式 和部分的快捷键 后者使用xml编写 和.rc文件一起用于规定窗口的视觉样式。\
+Data文件夹下的Cell.txt用于存放存档数据。Build下的.o文件是编译过程生成的目标文件。\n\
+    该程序的实现思路是 首先定义了一个细胞体类 它包含了细胞的位置 状态 财富值 颜色等信息 对于细胞财富Wealth在视觉上的展现 使用RGB来表现:对于一定的Wealth值 其颜色是RGB(max(0 255-Wealth) min(255 max(0 510-Wealth) min(255 max(0 765-Wealth)) \
+由此可以看出一个细胞财富最大值是765 最小值为0 越富有的细胞颜色越接近黑色 越贫穷的越接近白色。在由贫穷到富有的变化过程中 颜色是从白色到蓝色再变为黑色。整个可以绘制的版图是50x50 屏幕放不下 因此添加了垂直和水平滚动条 \
+窗口被分成了两块部分 左边是数据区 右边是演化区.由于精力有限 没考虑窗口大小变化时的窗口布局 因此干脆通过拦截相应的消息把窗口设置成无法调节大小的状态。\n     \
+在演化过程里 如果一个细胞想对另外一个细胞产生影响 那么它会通过这两个细胞之间的一条最短路径来传递影响 因此在演化开始前 会使用CreateThread函数来创建一个线程(线程函数就是Calcution函数) 这个线程的功能是使用广度优先计算出任意两个活细胞之间的最短路径.然后将结果存放于一个邻接矩阵ChannelGraph里 \
+这个零阶矩阵矩阵是一个全局变量 在演化过程中 每个细胞都可以访问到这个矩阵 从而可以快速查询它和其他活细胞细胞之间的最短路径 从而可以对其他细胞产生影响。两个活细胞之间的关系不仅仅是最短路径 还有承诺值(范围是0-1) 这个承诺值是在演化过程里动态变化的 为了可以快速访问它 \
+这里依然采用了邻接矩阵来记录它。而该细胞自动机的核心是活细胞的不断演化 该任务是由另外一个线程来完成的 线程函数是Evolution函数 它的功能是不断地进行活细胞之间的交互 程序设置的是每0.1秒进行一次演化 并刷新一次页面。演化的过程是 随机挑选五分之一的活细胞作为演化的发起者 然后根据题目里的规定从\
+其他活细胞里挑选出属于它的最优对象 进行交互 至于交互的细节 见题目。\n\
+    之所以计算和演化开辟了两个线程 是因为这两项工作都是耗时的工作 会长时间阻塞主线程。主线程主要进行的GUI的渲染 通过与主线程GUI的交互 可以随时控制其他连个线程的开始与停止\
+在本程序的实现过程里 涉及到了线程安全 因为这三个线程在同时工作的过程里会程序同时访问需改多个全局变量的情况 这个使用EnterCriticalSection和LeaveCriticalSection来解决。")
 
-#define TOOL TEXT("clang -v 19.1.0 (x86_64-pc-windows-msv)\nVisual Studio Code\nWindows API\n")
+#define TOOL TEXT("g++ -v 13.1.0 (x86_64-posix-seh-rev1)\nVisual Studo Code\nWindows API\nC++11\n")
 #define ID_BUTGO 1
 #define ID_BUTSTOP 2 
 #define ID_BUTERASE 3
