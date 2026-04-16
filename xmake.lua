1bad4db07f403f9102e33821a34fcf6c8e230b14
czt_arch <changzhentao@gmail.com>
2026-04-17 02:03:41 +0800

    对于xmake c/c++项目配置得更合理了, 1. build目录不应该是最后的软件件输出目录,而是中间产物的目录，改为默认 2. install目录才是最终的软件件输出目录，改为.vscode/bin 3. 对于外部依赖做了一个详细的规范:         依赖名称/include         依赖名称/lib

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,5 @@
 set_config("buildir", ".vscode/build")
+
 add_requires("avcodec","avformat","avutil","swscale","opencv4")
 
 -- 定义交叉工具链
@@ -15,12 +16,17 @@ target("App")
     add_includedirs("include")
     add_files("main.cpp","src/*.cpp")
 
+    if not is_mode("cross") then
+        add_packages("avcodec","avformat","avutil","swscale","opencv4")
+    end
+
     if is_mode("debug") then --明确指定debug模式
         -- 添加DEBUG编译宏
         add_defines("DEBUG")
         -- 设置目标文件存放目录
-        set_targetdir(".vscode/bin/debug")
-        add_packages("avcodec","avformat","avutil","swscale","opencv4")
+        -- set_targetdir(".vscode/bin/debug")
+
+        set_installdir("$(scriptdir)/.vscode/bin/$(arch)/${mode}")
         -- 启用调试符号
         set_symbols("debug")
         -- 禁用优化
@@ -29,31 +35,49 @@ target("App")
 
     if is_mode("release") then
         -- 设置目标文件存放目录
-        set_targetdir(".vscode/bin/release")        
-        add_packages("avcodec","avformat","avutil","swscale","opencv4")
+        -- set_targetdir(".vscode/bin/release")  
+
+        set_installdir("$(scriptdir)/.vscode/bin/$(arch)/${mode}")      
         set_optimize("fastest")
     end
 
     if is_mode("cross") then
-        -- 方便查看信息,改为.so后缀
-        set_filename("App.so")
-
-        set_optimize("fastest")
-
-        -- 设置目标文件存放目录
-        set_targetdir(".vscode/bin/cross")
         set_toolchains("aarch64")
 
-        add_includedirs("./res/opencv/include/opencv4")
-        add_linkdirs("./res/opencv/lib")
-        add_links("opencv_core","opencv_videoio","opencv_imgproc","opencv_imgcodecs")
+        local target_plat="linux"
+        local target_arch="aarch64"
+        local depsdir = "$(scriptdir)/res/deps/"
+        -- 依赖名称
+        local deps={"opencv","ffmpeg","rknn"}
 
-        -- 此为静态链接库
-        -- add_links("opencv_world")
-        -- add_linkdirs("./res/opencv/lib/opencv4/3rdparty")
-        
-        add_includedirs("./res/ffmpeg/include")
-        add_linkdirs("./res/ffmpeg/lib")
-        add_links("avcodec","avdevice","avformat","avfilter","avutil","swresample","swscale")
+        set_plat(target_plat)
+        set_arch(target_arch)
+        -- 方便查看信息,改为.so后缀
+        set_filename("Orangepi.so")
+
+        -- 添加依赖库的包含目录和链接目录
+        for _, dep in ipairs(deps) do
+             add_includedirs(depsdir..dep.."/include/")
+             add_linkdirs(depsdir..dep.."/lib/")
+        end
+
+        -- 编译连接配置
         add_rpathdirs("$ORIGIN/lib",{runpath = true})
+        set_optimize("fastest")
+        add_links("opencv_core","opencv_videoio","opencv_imgproc","opencv_imgcodecs")
+        add_links("avcodec","avdevice","avformat","avfilter","avutil","swresample","swscale")
+
+        -- 设置安装目录结构
+        set_installdir("$(scriptdir)/.vscode/bin/$(mode)/")
+        set_prefixdir("",{bindir=""})
+
+        -- 添加安装运行依赖文件
+        for _, dep in ipairs(deps) do
+            add_installfiles(depsdir..dep.."/lib/*", {prefixdir = "lib"})
+        end
+
+        -- 添加资源文件
+        add_installfiles("$(scriptdir)/res/model/*", {prefixdir="model"})
+        add_installfiles("$(scriptdir)/res/testvideos/*",{prefixdir="testvideos"})
+
     end
