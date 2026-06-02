3f5207ab97f63a2b2717ae3ed290c8a676a1404e
czt_arch <changzhentao@gmail.com>
2026-06-02 22:26:12 +0800

    改进了工程结构

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,9 @@
 set_config("buildir", ".vscode/build")
 add_rules("mode.debug", "mode.release")
 
-target("Temp")
+target("App")
+    set_toolset("clang")
     add_rules("qt.widgetapp")
     add_files("src/*.cpp")
-    add_files("src/widget.ui")
-    -- add files with Q_OBJECT meta (only for qt.moc)
+    add_files("UI/widget.ui")
     add_files("src/widget.h")
